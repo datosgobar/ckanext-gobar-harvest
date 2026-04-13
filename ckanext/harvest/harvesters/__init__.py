91bba4b1f11b81bf70d7b8aa7b59ad7e59c960aa
alejandrafau <maria.alejandra.fauquie@gmail.com>
2026-04-13 16:36:07 -0300

    Agrega harvester con xlsx y cambia el __init__.py en harvesters

--- ckanext/harvest/harvesters/__init__.py
+++ ckanext/harvest/harvesters/__init__.py
@@ -1,4 +1,5 @@
 from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
 from ckanext.harvest.harvesters.base import HarvesterBase
+from .xlsxharvester import XLSXHarvester
 
-__all__ = ['CKANHarvester', 'HarvesterBase']
+__all__ = ['CKANHarvester', 'HarvesterBase','XLSXHarvester']
